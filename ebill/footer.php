459c63d4c1c38eb99282760bc1e2bc011db4017f
Harshalnakshine <harshalnakshine445@gmail.com>
2024-07-03 10:00:54 +0530

    Add files via upload

---
+++ ebill/footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+<hr>
+<p class="centered">Created by <a href="https://www.softsense.ind.in/">SOFTSENCE</a> Developer</b></p>
+</div>
---
+++ ebill/user/sidebar.php
@@ -0,0 +1,17 @@
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="active">
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <!-- new bills by bootstrap badges -->
+        <li>
+            <a href="coupon.php"><i class="fa fa-fw fa-info"></i> Apply Coupon </a>
+        </li>
+        <li>
+            <a href="complaint.php"><i class="fa fa-fw fa-info"></i> Complaints </a>
+        </li>
+        <!-- processed bills as bootstrap badges -->
+    </ul>
+</div>
+<!-- /#sidebar-wrapper -->
